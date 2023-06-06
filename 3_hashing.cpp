a60b7da518cc9a45364f66e6168ed48df3d083cd
Mausam <112643319+mausam30@users.noreply.github.com>
2023-06-06 22:11:20 +0530

    Update 3_hashing.cpp

--- 3_hashing.cpp
+++ 3_hashing.cpp
@@ -3,9 +3,9 @@
 #include <random>
 
 int custom_hash(const std::string& message) {
-    std::hash<std::string> hash_fn;  // Instantiate the hash function object
-    std::size_t hash_value = hash_fn(message);  // Compute the hash value
-    return static_cast<int>(hash_value & 0xFFFF);  // Extract the lower 16 bits
+    std::hash<std::string> hash_fn;  
+    std::size_t hash_value = hash_fn(message);  
+    return static_cast<int>(hash_value & 0xFFFF);  
 }
 
 int main() {
@@ -19,7 +19,7 @@ int main() {
     std::cout << "Original Message: " << message << std::endl;
     std::cout << "Hash value: " << hash_value << std::endl;
 
-    // Simulate receiver computing the hash again
+    
     std::string received_message;
     std::cout << "Enter the received message: ";
     std::getline(std::cin, received_message);
@@ -29,7 +29,7 @@ int main() {
     std::cout << "Received Message: " << received_message << std::endl;
     std::cout << "Received Hash value: " << received_hash_value << std::endl;
 
-    // Verify integrity
+
     if (received_hash_value == hash_value) {
         std::cout << "Integrity: The message has not been modified." << std::endl;
     } else {
